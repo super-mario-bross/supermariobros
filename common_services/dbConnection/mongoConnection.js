5950bfaa1bd8a930e3ce7b67fe77bd662a4e47e8
vipul shukla <vipulshukla@vipuls-MacBook-Pro.local>
2020-09-25 10:18:27 +0530

    fixes mongo url

--- common_services/dbConnection/mongoConnection.js
+++ common_services/dbConnection/mongoConnection.js
@@ -3,10 +3,9 @@ let mongoOptions = {
   server: {poolSize: 20},
   useNewUrlParser: true
 };
-const mongoConnectionURL = 'mongodb+srv://supermario:supermario@cluster0.naqdh.mongodb.net/supermario?retryWrites=true&w=majority';
+const mongoConnectionURL = 'mongodb://localhost:27017/supermario';
 MongoClient.connect(mongoConnectionURL, mongoOptions, function (err, database) {
   if (err) {
-    console.log(">>>>>>>>>>>>>>",err)
     console.error({origin: 'MONGO CONNECTION', event:'Trying connecting mongodb!!', message: {mongoConnectionURL, mongoOptions}});
   } else {
     console.log({origin: 'MONGO CONNECTION', event:'Connection established with mongodb!!', message: {mongoOptions}});
