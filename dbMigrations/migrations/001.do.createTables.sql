b298508ab89aa4cf9009cbfd8eb0fd4626fd9c9d
vipul shukla <vipulshukla@vipuls-MacBook-Pro.local>
2020-09-26 11:21:36 +0530

    added ratings table

--- dbMigrations/migrations/001.do.createTables.sql
+++ dbMigrations/migrations/001.do.createTables.sql
@@ -13,3 +13,20 @@ WITH (
 
 CREATE UNIQUE INDEX entity_unique_idx ON entities (entity_id);
 
+
+CREATE TABLE ratings (
+	uuid UUID NOT NULL,
+	entity_id varchar(50) NOT NULL,
+    title varchar(50) NOT NULL,
+	review_dec varchar(50) NOT NULL,
+	rating float NOT NULL,
+	is_helpful BOOLEAN,
+	sentiment_score integer NOT NULL,
+	updated_at timestamp with time zone DEFAULT timezone('UTC'::text, CURRENT_TIMESTAMP),
+	deleted_at timestamp with time zone  DEFAULT NULL,
+    created_at timestamp with time zone DEFAULT timezone('UTC'::text, CURRENT_TIMESTAMP),
+	CONSTRAINT rating_pkey PRIMARY KEY (uuid)
+)
+WITH (
+	OIDS=FALSE
+);
